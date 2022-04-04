bcea1638dfac2823f57d5d8da11648f247132e63
Jacob953 <jacob953@csu.edu.cn>
2022-04-04 15:47:31 +0800

    feat: --20220404 update proxy on dev-go

---
+++ go/basic/proxy/v1/receive_test.go
@@ -0,0 +1,19 @@
+package v1
+
+func ExampleReceive() {
+	tony := &TonyReception{
+		name:     "Tony",
+		phoneNum: "18512345678",
+	}
+	wendy := &WendyReception{
+		name:     "Wendy",
+		receiver: tony,
+	}
+	wendy.receive("雪地靴")
+
+	// Output:
+	// 我是Tony的朋友， 我来帮他代收快递！
+	// 货物主人：Tony， 手机号：18512345678
+	// 接收到一个包裹，包裹内容：雪地靴
+	// 代收人：Wendy
+}
